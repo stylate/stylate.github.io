45a6c9ff4ded7514c0176080a7c8eb321387970a
Alan Nguyen <nguyenalan@berkeley.edu>
2019-01-22 12:39:02 -0800

    updated now page

--- src/pages/now.js
+++ src/pages/now.js
@@ -24,11 +24,11 @@ export default class Now extends React.Component {
             <h3>Website Redesign</h3>
             <p>This website's codebase is at the moment very, very messy (and outdated)! I've been using Gatsby and CSS Modules to maintain this website and its aesthetics. </p>
             
-            <p>Over the span of this ongoing semester, I'm in the process of learning more front-end web development for personal enjoyment, while simultaneously switching to styled components, Jekyll, and React and building the website from scratch!</p>
+            <p>I'm currently in the process of learning more front-end web development for personal enjoyment, while simultaneously switching to styled components, Jekyll, and React and building the website from scratch!</p>
             <p>I also plan on completely separating my photography from this personal website in order to maintain full creative control over their respective web designs without compromise.</p>
 
             <h3>Education</h3>
-            <p>This semester is currently dedicated to me being engaged in more project-based work, as well as tutoring! Aside from being involved with <a href="https://csmberkeley.github.io/#/">CS Mentors</a> for UC Berkeley's introductory Data Structures course, I'm enrolled in a series of project-based computer science courses:</p>
+            <p>This semester is currently dedicated to me being engaged in more project-based work, as well as tutoring! I'm enrolled in a series of project-based computer science courses:</p>
             <ul>
                 <li>Computer Graphics</li>
                 <li>Computer Security</li>
@@ -42,7 +42,7 @@ export default class Now extends React.Component {
             <p>One of my fun facts is that my Spotify playlists are color coded based on album cover and the "mood" that they feed off. I've been working with a team of three to turn this cool, super extra idea into code. Feel free to track its progress <a href="https://github.com/Kaden-Dippe/spotify-colorized">here</a>.</p>
 
             <h3>Photo/Film</h3>
-            <p>I'm currently co-leading a photography team as part of <a href="https://innovativedesign.club/">Innovative Design</a>, as well as making an attempt to visit a lot of older directors and their work.</p>
+            <p>I'm currently co-leading a photography team as part of <a href="https://innovativedesign.club/">Innovative Design</a>, as well as making an attempt to visit a lot of older directors and their work. Check out the past movies I've watched <a href="https://letterboxd.com/carbine/">here</a>!!</p>
             </CSSTransitionGroup>
             </div>
         </div>
