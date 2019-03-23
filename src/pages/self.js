6aa0fed21a122c137cd70623702d0b86b1bd9e6e
Alan Nguyen <nguyenalan@berkeley.edu>
2019-03-22 23:33:13 -0700

    remove /decal from public view; introduce new project /self

---
+++ src/pages/self.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Sidebar from '../layouts/sidebar'
+
+import intro from '../content/photo/self/intro.jpg'
+import m1 from '../content/photo/self/1.jpg'
+import m2 from '../content/photo/self/2.jpg'
+import m3 from '../content/photo/self/3.jpg'
+import m4 from '../content/photo/self/4.jpg'
+import m5 from '../content/photo/self/5.jpg'
+import m6 from '../content/photo/self/6.jpg'
+import m7 from '../content/photo/self/7.jpg'
+import m8 from '../content/photo/self/8.jpg'
+import m9 from '../content/photo/self/9.jpg'
+import m10 from '../content/photo/self/10.jpg'
+import m11 from '../content/photo/self/11.jpg'
+import m12 from '../content/photo/self/12.jpg'
+import m13 from '../content/photo/self/13.jpg'
+import m14 from '../content/photo/self/14.jpg'
+import outro from '../content/photo/self/outro.jpg'
+
+
+export default class Green extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <Sidebar/>
+                <div className="wrapper-img">
+                    <img src={intro} alt={"intro"}/>
+                    <img src={m1} alt={"m1"}/>
+                    <img src={m2} alt={"m2"}/>
+                    <img src={m3} alt={"m3"}/>
+                    <img src={m4} alt={"m4"}/>
+                    <img src={m5} alt={"m5"}/>
+                    <img src={m6} alt={"m6"}/>
+                    <img src={m7} alt={"m7"}/>
+                    <img src={m8} alt={"m8"}/>
+                    <img src={m9} alt={"m9"}/>
+                    <img src={m10} alt={"m10"}/>
+                    <img src={m11} alt={"m11"}/>
+                    <img src={m12} alt={"m12"}/>
+                    <img src={m13} alt={"m13"}/>
+                    <img src={m14} alt={"m14"}/>
+                    <img src={outro} alt={"outro"}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+
